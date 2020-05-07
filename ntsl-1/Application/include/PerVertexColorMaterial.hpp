025d7ed89ec0f485218995acf9e90226af2c9a4f
tink-expo <whhan8485@gmail.com>
2020-05-08 05:43:32 +0900

    Cleanup files

--- ntsl-1/Application/include/PerVertexColorMaterial.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <Material.hpp>
-
-class PerVertexColorMaterial : public Engine::Material
-{
-public:
-    void CreateMaterial();
-};
--- ntsl-1/Application/src/PerVertexColorMaterial.cpp
+++
@@ -1,8 +0,0 @@
-#include <PerVertexColorMaterial.hpp>
-
-void PerVertexColorMaterial::CreateMaterial()
-{
-    _program->AttachShader("Resources/PerVertexColorMaterial/VertexShader.glsl", GL_VERTEX_SHADER);
-    _program->AttachShader("Resources/PerVertexColorMaterial/FragmentShader.glsl", GL_FRAGMENT_SHADER);
-    _program->LinkShader();
-}
